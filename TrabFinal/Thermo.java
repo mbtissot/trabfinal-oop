02ab9c4a2ab1a1759d2ce15ce688c1594f2d8bfa
Zarauto <108766073+Zarauto@users.noreply.github.com>
2023-03-21 13:17:56 -0300

    Adicionar temperaturas minima e maxima

--- TrabFinal/Thermo.java
+++ TrabFinal/Thermo.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class Thermo here.
+ * Classe para dispositivos com temperatura controlada (ares condicionados, fornos, geladeiras, etc.)
  *
  * @author (your name)
  * @version (a version number or a date)
@@ -9,14 +9,18 @@ public class Thermo extends Device
 {
     // instance variables - replace the example below with your own
     private int temperature;
+    private int tMin;
+    private int tMax;
 
     /**
      * Constructor for objects of class Thermo
      */
-    public Thermo(String name)
+    public Thermo(String name, int tMin, int tMax)
     {
-        super(name);
-        this.temperature = 0;
+        super(name);     
+        this.tMax = tMax;
+        this.tMin = tMin;
+        this.temperature = (tMin+tMax)/2;
     }
 
     public int getTemp(){
@@ -25,6 +29,22 @@ public class Thermo extends Device
     
     public void setTemp(int temp){
         this.temperature = temp;    
+    }
+    
+    public int getTMin(){
+        return this.tMin;
+    }
+    
+    public void setTMin(int t){
+        this.tMin = t;
+    }
+    
+    public void setTMax(int t){
+        this.tMax = t;
+    }
+    
+    public int getTMax(){
+        return this.tMax;
     }
     
     public String toString() {
